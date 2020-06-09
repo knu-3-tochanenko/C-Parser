2c2f0ef22bf8158895739b979da72174bd49b336
Vladislav <itochanenko@gmail.com>
2020-06-09 17:55:14 +0300

    Add helper methods

--- Parser/src/main/java/Parser.java
+++ Parser/src/main/java/Parser.java
@@ -1,3 +1,5 @@
+import java.util.Vector;
+
 public class Parser {
     int position = 0;
     String noStatement = "<no statement>";
@@ -5,5 +7,35 @@ public class Parser {
     private void assertToken(Token token, String s) throws Exception {
         if (!token.content().equals(s))
             throw new Exception(String.format("%s expected but token %d value is %s", s, position, token.content()));
+    }
+
+    Node parse(Vector<Token> tokens) throws Exception {
+        return parseProgram(tokens);
+    }
+
+    private Node parseProgram(Vector<Token> tokens) throws Exception {
+        // TODO
+        return null;
+    }
+
+    private boolean isEqualityToken(Token token) {
+        String content = token.content();
+        return content.equals("==") || content.equals("!=") || content.equals("<=") || content.equals(">=") ||
+                content.equals("<") || content.equals(">");
+    }
+
+    private boolean isAdditiveToken(Token token) {
+        String content = token.content();
+        return content.equals("+") || content.equals("-");
+    }
+
+    private boolean isMultiplicativeToken(Token token) {
+        String content = token.content();
+        return content.equals("*") || content.equals("/") || content.equals("%");
+    }
+
+    private boolean isUnaryToken(Token token) throws Exception {
+        String content = token.content();
+        return content.equals("+") || content.equals("-") || content.equals("~") || content.equals("!");
     }
 }
